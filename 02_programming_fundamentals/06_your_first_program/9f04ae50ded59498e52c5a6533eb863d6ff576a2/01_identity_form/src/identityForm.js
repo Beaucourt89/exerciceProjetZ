0830e053249887dd520037d3235ac7eab4e19622
Beaucourt89 <beaucourt.pro@gmail.com>
2020-09-15 10:20:00 +0200

    first exo identityForm

---
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/01_identity_form/src/identityForm.js
@@ -0,0 +1,13 @@
+const identityForm = (reader) => {
+
+  reader.question("first name ", (name) => {
+    reader.question("last name", (lastname) => {
+      reader.question("What is your age? ", (age) => {
+        console.log(`Your name is ${name} ${lastname} and you are ${age}.`);
+        reader.close();
+      });
+    });
+  });
+};
+
+module.exports = identityForm;
