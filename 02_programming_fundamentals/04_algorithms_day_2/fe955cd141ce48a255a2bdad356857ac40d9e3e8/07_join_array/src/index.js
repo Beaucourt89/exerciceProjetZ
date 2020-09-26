2d79041d59b6083940092b7d64e880d71275030b
Beaucourt89 <beaucourt.pro@gmail.com>
2020-09-26 16:19:39 +0200

    Join array

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/07_join_array/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/07_join_array/src/index.js
@@ -1,6 +1,15 @@
-function joinArray() {
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const separateur = " <> ";
+
+function joinArray(param1, param2) {
   // Code your function here.
+
+  return param1.reduce(
+    (accumulator, currentValue) =>
+      accumulator + param2 + currentValue
+  );
 }
+console.log(joinArray(array1, separateur));
 
 // ⚠ Do not remove me ! It's for tests
 module.exports = joinArray;
