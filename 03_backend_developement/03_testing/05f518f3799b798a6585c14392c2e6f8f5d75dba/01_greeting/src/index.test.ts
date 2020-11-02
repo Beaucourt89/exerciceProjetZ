12a24f2897f770b537a5e14142a6188b182f71bb
Timothee Beaucourt <timotheebeaucourt@mbp-de-timothee.home>
2020-11-02 11:38:34 +0100

    exo

--- 03_backend_developement/03_testing/05f518f3799b798a6585c14392c2e6f8f5d75dba/01_greeting/src/index.test.ts
+++ 03_backend_developement/03_testing/05f518f3799b798a6585c14392c2e6f8f5d75dba/01_greeting/src/index.test.ts
@@ -1,18 +1,35 @@
 
 import {greet} from "./index";
 
-  test("retrun Hello WORLD if name empty ", function() {
-    expect.assertions(1);
 
-    const result = greet();
 
-    expect(result).toBe("Hello WORLD!");
-  });
+test("return Hello World if name empty", function() {
+  const result = greet();
+
+  expect(result).toBe("Hello WORLD!");
+});
+
+
+test("return Hello World if name empty", function() {
+  const result = greet("toto");
+
+  expect(result).toBe("Hello TOTO!");
+});
+
+
+  // test("retrun Hello WORLD if name empty ", function() {
+
+  //   expect.assertions(1);
+
+  //   const result = greet();
+
+  //   expect(result).toBe("Hello WORLD!");
+  // });
   
-  test("return Hello name uppercase", function() {
-    expect.assertions(1);
+  // test("return Hello name uppercase", function() {
+  //   expect.assertions(1);
 
-    const result = greet("toto");
+  //   const result = greet("toto");
     
-    expect(result).toBe("Hello TOTO!");
-  });
+  //   expect(result).toBe("Hello TOTO!");
+  // });
--- 03_backend_developement/07_mongo_advanced/74804e3c761353384151cb20f565646bba6cf592/01_import_data/src/dataImport.ts
+++ 03_backend_developement/07_mongo_advanced/74804e3c761353384151cb20f565646bba6cf592/01_import_data/src/dataImport.ts
@@ -5,11 +5,9 @@ import { Db } from "mongodb";
 type Platform = Record<string, unknown> & {
   games: Record<string, unknown>[];
 };
-
 type Game = {
   [key: string]: unknown;
 };
-
 //Pour lire les infos du fichier Json
 function getDataFromFile(fileName: string): [Game[], Platform] {
   const filePath = path.resolve(`data/${fileName}`);
@@ -20,26 +18,21 @@ function getDataFromFile(fileName: string): [Game[], Platform] {
   const { games, ...platformWithoutGames } = platform;
   return [games, platformWithoutGames];
 }
-
 //CET FONCTION NE FAIT QU IMPORTER DE LA DONNÉE
 //comme dans le fichier index la function dataImport est Associée a .then
 //nous savons que c une promise
 export default function dataImport(db: Db): Promise<boolean> {
   let allGames: Game[] = [];
   const allPlatforms: Platform[] = [];
-
-  //prend le dossier data qui Contient nes, n-64 et gba 
+  //prend le dossier data qui Contient nes, n-64 et gba
   const dirPath = path.resolve("data");
   const fileNames = fs.readdirSync(dirPath);
-
-
 
   fileNames.forEach((fileName) => {
     const [games, platform] = getDataFromFile(fileName);
     allPlatforms.push(platform);
     allGames = [...allGames, ...games];
   });
-
   return db.createCollection<Platform>("platforms").then((platformCollection) => {
     return db.createCollection<Game>("games").then((gamesCollection) => {
       return platformCollection.insertMany(allPlatforms).then(() => {
